84397e4c2fae77ecbdd25b365e8a91ed14d8e4a9
Matt Mazzola <mattm@microsoft.com>
2018-07-23 16:08:49 -0700

    setup Speech API using npm package
    https://www.npmjs.com/package/microsoft-speech-browser-sdk

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,81 @@
 import * as React from 'react'
+import * as SDK from 'microsoft-speech-browser-sdk'
+import * as SpeechService from './services/speechRecognizer'
 import './App.css'
 
-class App extends React.Component {
-  onClickTalk() {
+interface State {
+  isMicrophoneActive: boolean
+}
+
+class App extends React.Component<{}, State> {
+  recognizer: SDK.Recognizer
+  state = {
+    isMicrophoneActive: false
+  }
+
+  constructor(p: any) {
+    super(p)
+
+    this.recognizer = SpeechService.setup(SDK.RecognitionMode.Interactive, "en-US", SDK.SpeechResultFormat.Simple, "8e9795ad8f3e4e61a2a67498600d3922")
+  }
+
+  onClickTalk = () => {
     console.log(`onClickTalk: `)
+    SpeechService.start(this.recognizer, this.onEvent, this.onComplete, this.onError)
+  }
+
+  onComplete = () => {
+    console.log(`onComplete`)
+  }
+
+  onError = (error: Error) => {
+    console.error(error)
+    this.setState({
+      isMicrophoneActive: false
+    })
+  }
+
+  onEvent = (event: SDK.SpeechRecognitionEvent) => {
+    if (event instanceof SDK.RecognitionTriggeredEvent) {
+      console.log(`RecognitionTriggeredEvent: `)
+    }
+    else if (event instanceof SDK.ListeningStartedEvent) {
+      console.log(`ListeningStartedEvent: `)
+      this.setState({
+        isMicrophoneActive: true
+      })
+    }
+    else if (event instanceof SDK.RecognitionStartedEvent) {
+      console.log(`RecognitionStartedEvent: `)
+    }
+    else if (event instanceof SDK.SpeechStartDetectedEvent) {
+      console.log(`SpeechStartDetectedEvent: `)
+    }
+    else if (event instanceof SDK.SpeechHypothesisEvent) {
+      console.log(`SpeechHypothesisEvent: `)
+    }
+    else if (event instanceof SDK.SpeechFragmentEvent) {
+      console.log(`SpeechFragmentEvent: `)
+      console.log(JSON.stringify(event.Result, null, 2))
+    }
+    else if (event instanceof SDK.SpeechEndDetectedEvent) {
+      console.log(`SpeechEndDetectedEvent: `)
+      console.log(JSON.stringify(event.Result, null, 2))
+    }
+    else if (event instanceof SDK.SpeechSimplePhraseEvent) {
+      console.log(`SpeechSimplePhraseEvent: `)
+      console.log(JSON.stringify(event.Result, null, 2))
+    }
+    else if (event instanceof SDK.SpeechDetailedPhraseEvent) {
+      console.log(`SpeechDetailedPhraseEvent: `)
+      console.log(JSON.stringify(event.Result, null, 2))
+    }
+    else if (event instanceof SDK.RecognitionEndedEvent) {
+      console.log(`RecognitionEndedEvent: `)
+      this.setState({
+        isMicrophoneActive: false
+      })
+    }
   }
 
   render() {
@@ -12,9 +84,14 @@ class App extends React.Component {
         <div className="kl-canvas">
         </div>
 
-        <button className="kl-button-talk" onClick={this.onClickTalk}>
-          <i className="material-icons">mic</i>
-        </button>
+        <div className="kl-talk">
+          <button className={`kl-button-talk ${this.state.isMicrophoneActive ? 'kl-button-talk--active' : ''}`} onClick={this.onClickTalk}>
+            <i className="material-icons">mic</i>
+          </button>
+          {this.state.isMicrophoneActive
+            ? <div>Recording...</div>
+            : <div>Push To Talk</div>}
+        </div>
       </div>
     );
   }
