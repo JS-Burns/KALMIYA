44a0ff5a4d0c2af083453e345ba28ad547ba3865
JS-Burns <46832966+JS-Burns@users.noreply.github.com>
2019-01-28 20:11:57 -0500

    // RENEW KEY
    ~J/280119

--- src/services/imageSearch.ts
+++ src/services/imageSearch.ts
@@ -1,5 +1,5 @@
-
-
+// RENEW KEY
+// https://docs.microsoft.com/en-us/azure/cognitive-services/bing-web-search/quick-start
 const baseUri = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
 const subscriptionKey = "cb1b401c417d45be9c7c4da982ea7128"
 
